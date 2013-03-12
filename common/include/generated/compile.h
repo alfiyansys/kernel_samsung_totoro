9adb33bd1bbe160245271b54f9b148cba4961920
M Alfiyan S <alfiyan.syamsuddin@gmail.com>
2013-03-12 11:00:49 +0700

    IanKernel-v1.33, sept2012 initial

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 133 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#133 PREEMPT Sun Sep 16 19:11:37 WIT 2012"
+#define LINUX_COMPILE_TIME "19:11:37"
+#define LINUX_COMPILE_BY "alfiyan"
+#define LINUX_COMPILE_HOST "regulus-arcturus-labs"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
---
+++ common/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.35.7-IanKernel-Young-v1.30-rev2"
